6e390ec02fe596ff701f3b31d29311a9cf3a9c40
UliaF80 <uliafedorova297@cmail.com>
2022-07-22 15:17:17 +0300

    add all files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 7_500;
+
+        int bonus = ticketPrice / 20;
+        System.out.println("бонусные милли" + bonus);
+    }
+}
